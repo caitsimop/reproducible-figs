775e0e9787be3dfff6f1d0a088edeed49fc64c7e
Caitlin <caitlin.simopoulos@gmail.com>
2020-01-21 11:48:43 -0500

    Updating directory structure

---
+++ scripts/reproducible_figs.R
@@ -0,0 +1,7 @@
+### Script explaining how and why to make reproducible figures
+
+### loading R packages 
+library(tidyverse) #install.packages("tidyverse") # this one takes a long time to install...
+#library(patchwork) #devtools::install_github("thomasp85/patchwork")
+library(limma)
+## User inputs
